6bba89c379debe56d345ec909c7dad7ca04217ef
saleempirwani <msr.pirwani.dev@gmail.com>
2024-12-23 17:05:05 +0500

    api-call updated

--- api-call.js
+++ api-call.js
@@ -1,8 +1,9 @@
-// 1. Create User (POST)
-fetch("http://66.179.208.43:8080/api/v1/user/create", {
+// Create User (POST)
+fetch("http://66.179.208.43/api/v1/user/create", {
   method: "POST",
   headers: {
     "Content-Type": "application/json",
+    Authorization: "a1B2c3D4e5F6g7H8i9J0kLmNoPqRsTu",
   },
   body: JSON.stringify({
     firstName: "John",
@@ -14,40 +15,86 @@ fetch("http://66.179.208.43:8080/api/v1/user/create", {
   }),
 })
   .then((response) => response.json())
-  .then((data) => console.log("User created:", data))
+  .then((data) => console.log(data))
   .catch((error) => console.error("Error:", error));
 
-//   2. Update User (PATCH)
-fetch("http://66.179.208.43:8080/api/v1/user/update/676559390f61f32ea2a45094", {
+//  Update User (PATCH)
+fetch("http://66.179.208.43/api/v1/user/update/6766e66d7c0a8cd4413fe2ec", {
   method: "PATCH",
   headers: {
     "Content-Type": "application/json",
+    Authorization: "a1B2c3D4e5F6g7H8i9J0kLmNoPqRsTu",
   },
   body: JSON.stringify({
     firstName: "John",
   }),
 })
   .then((response) => response.json())
-  .then((data) => console.log("User updated:", data))
+  .then((data) => console.log(data))
   .catch((error) => console.error("Error:", error));
 
-//   3. Delete User (DELETE)
-fetch("http://66.179.208.43:8080/api/v1/user/delete/676559390f61f32ea2a45094", {
+//   Delete User (DELETE)
+fetch("http://66.179.208.43/api/v1/user/delete/67670154fb724d2739455cbf", {
   method: "DELETE",
-})
-  .then((response) => {
-    if (response.ok) {
-      console.log("User deleted successfully");
-    } else {
-      console.error("Failed to delete user");
-    }
-  })
-  .catch((error) => console.error("Error:", error));
-
-// 4. Get All Users with Role (GET)
-fetch("http://66.179.208.43:8080/api/v1/user/all/business", {
-  method: "GET",
+  headers: {
+    Authorization: "a1B2c3D4e5F6g7H8i9J0kLmNoPqRsTu",
+  },
 })
   .then((response) => response.json())
-  .then((data) => console.log("Users with role 'business':", data))
+  .then((data) => console.log(data))
+  .catch((error) => console.error("Error:", error));
+
+// Get User Details
+fetch("http://66.179.208.43/api/v1/user/details/6766e66d7c0a8cd4413fe2ec", {
+  method: "GET",
+  headers: {
+    Authorization: "a1B2c3D4e5F6g7H8i9J0kLmNoPqRsTu",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error("Error:", error));
+
+// Get All Users
+fetch("http://66.179.208.43/api/v1/user/all", {
+  method: "GET",
+  headers: {
+    Authorization: "a1B2c3D4e5F6g7H8i9J0kLmNoPqRsTu",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error("Error:", error));
+
+// Get All Users By Role
+fetch("http://66.179.208.43/api/v1/user/all?role=business", {
+  method: "GET",
+  headers: {
+    Authorization: "a1B2c3D4e5F6g7H8i9J0kLmNoPqRsTu",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error("Error:", error));
+
+// Get All Users By Username
+fetch("http://66.179.208.43/api/v1/user/all?username=elladavis05", {
+  method: "GET",
+  headers: {
+    Authorization: "a1B2c3D4e5F6g7H8i9J0kLmNoPqRsTu",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
+  .catch((error) => console.error("Error:", error));
+
+// Get All Users By Email
+fetch("http://66.179.208.43/api/v1/user/all?email=johndenis23@gmail.com", {
+  method: "GET",
+  headers: {
+    Authorization: "a1B2c3D4e5F6g7H8i9J0kLmNoPqRsTu",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => console.log(data))
   .catch((error) => console.error("Error:", error));
